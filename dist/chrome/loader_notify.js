dfddb96ea0a74e520ed22a7f96388abd41a4e763
ExplodingCamera <mail@explodingcamera.com>
2016-02-14 20:35:06 +0100

    Manipulation of WebSockets
    This isn't actualy live yet but it's amazing! Endless posibilities!

--- dist/chrome/loader_notify.js
+++ dist/chrome/loader_notify.js
@@ -4,6 +4,15 @@ function fn() {
       window.MPmentionSound = new window.AudioBackup('https://explodingcamera.xyz/plop.mp3');
       return window.MPmentionSound;
     }
+    window.WebSocketBackup = window.WebSocket;
+    var sockets = 0;
+    var i = 0;
+    window.MPsocket = [];
+    window.WebSocket = function (val) {
+      i++;
+      window.MPsocket[i] = new WebSocketBackup(val);
+      return window.MPsocket[i];
+    }
 }
 var script = document.createElement('script')
 script[script.innerText ? 'innerText' : 'textContent'] = '(' + fn + ')()';
